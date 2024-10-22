bd7bcb0a9cd15407401f80384bf914de2206d164
saputra1377 <saputrasrof@gmail.com>
2024-10-23 03:52:35 +0700

    progres

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -3,6 +3,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserModel extends Model
 {
@@ -11,5 +12,10 @@ class UserModel extends Model
     protected $table = 'm_user';
     protected $primarykey = 'user_id';
 
-    protected $fillable = ['level_id', 'username', 'nama'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
